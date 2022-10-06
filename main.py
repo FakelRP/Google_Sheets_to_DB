b18987fd964785d66d333b2aeeb4812d19a207fe
Daniil <petredanil@mail.ru>
2022-10-06 16:25:42 +0300

    The minimum test task has been completed

--- main.py
+++ main.py
@@ -1,21 +1,49 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials as sac
 import pandas as pd
-import sqlalchemy as sa
+from sqlalchemy import create_engine
+import requests
+from bs4 import BeautifulSoup
+from schedule import every, repeat, run_pending
+import time
 
 
-def gsheet2df(spreadsheet_name, sheet_num):
+def get_usd_rate():
+    url = 'https://www.cbr.ru/'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'lxml')
+    usd = soup.find_all('div', class_='col-md-2 col-xs-9 _right mono-num')
+    str_usd = str(usd[1]).replace(',', '.')
+    usd_float = float(str_usd[47:54])
+    return usd_float
+
+
+def google_sheet_to_df(spreadsheet_name, sheet_num):
     scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
     credentials_path = 'credentials.json'
-
     credentials = sac.from_json_keyfile_name(credentials_path, scope)
     client = gspread.authorize(credentials)
-
     sheet = client.open(spreadsheet_name).get_worksheet(sheet_num).get_all_records()
     df = pd.DataFrame.from_dict(sheet)
+    return df
 
+
+@repeat(every(30).seconds)
+def df_to_db():
+    engine = create_engine("postgresql+psycopg2://postgres:Petre535@127.0.0.1:5433/kanalservice")
+    df = google_sheet_to_df('kanal_test', 0)
+    usd_rate = get_usd_rate()
+    price_rub = []
+    for i in range(len(df)):
+        df_price_usd = df.iloc[i]['стоимость_$']
+        price_in_usd = "{:.2f}".format(df_price_usd * usd_rate)
+        price_rub.append(price_in_usd)
+    df.insert(loc=4, column='стоимость_руб', value=price_rub)
     print(df)
+    df.to_sql('orders', engine, if_exists='replace', index=False)
 
-    return df.head()
 
-gsheet2df('kanal_test', 0)
+if __name__ == '__main__':
+    while True:
+        run_pending()
+        time.sleep(1)
